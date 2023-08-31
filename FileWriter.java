37b290ed5e0a40efe8d117a2902a59f18d1ad61f
Ryan Cheng <rz.cheng06@gmail.com>
2023-08-30 20:36:33 -0700

    added countCharacters method

--- FileWriter.java
+++ FileWriter.java
@@ -1,21 +1,44 @@
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.Scanner;
 
 public class FileWriter {
-    public String fileToString (String fileName) throws FileNotFoundException
-    {
-        Scanner scanner = new Scanner( new File(fileName) );
+    public String fileToString(String fileName) throws FileNotFoundException {
+        Scanner scanner = new Scanner(new File(fileName));
         String myString = scanner.useDelimiter("\\A").next();
         scanner.close();
         return myString;
     }
 
-    public void stringToFile (String input, String fileName) throws FileNotFoundException
-    {
+    public void stringToFile(String input, String fileName) throws FileNotFoundException {
         PrintWriter out = new PrintWriter(fileName);
         out.print(input);
         out.close();
     }
+
+    public int countCharacters(String fileName) {
+
+        try {
+            BufferedReader bufferedReader = new BufferedReader(new FileReader(fileName));
+            
+            int characterCount = 0;
+
+            while (bufferedReader.ready()) {
+                bufferedReader.read();
+                characterCount++;
+            }
+
+            bufferedReader.close();
+            return characterCount;
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            return -1;
+        }
+    }
+
 }
--- FileWriterTester.java
+++ FileWriterTester.java
@@ -1,11 +1,15 @@
 import java.io.FileNotFoundException;
+import java.io.IOException;
 
 public class FileWriterTester
 {
     public static void main (String [] args) throws FileNotFoundException
     {
         FileWriter myWriter = new FileWriter ();
+
         myWriter.stringToFile("Susmebit", "MyTextFile.txt");
         System.out.println(myWriter.fileToString ("MyTextFile.txt"));
+        System.out.println(myWriter.countCharacters("MyTextFile.txt"));
+        
     }
 }
